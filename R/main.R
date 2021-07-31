7b0c775fccd8b21ab638600dda538d18b8e39c87
Hiroaki Yutani <yutani.ini@gmail.com>
2021-07-31 21:25:38 +0900

    Add documents

--- R/main.R
+++ R/main.R
@@ -15,21 +15,48 @@
 #'
 #' @param line_width Line width of strokes.
 #'
-#' @return A `tibble()` containing these columns: \itemize{ \item{x}{Unscaled
-#'   position of x.} \item{y}{Unscaled position of y.} \item{glyph_id}{IDs to
-#'   distinguish the glyphs.} \item{path_id}{IDs to distinguish the groups of
-#'   paths.} \item{triangle_id}{IDs to distinguish the triangles.
-#'   `string2path()` doesn't contain this column.} }
+#' @return A `tibble()` containing these columns:
+#' \itemize{
+#'   \item{x}{Unscaled position of x.}
+#'   \item{y}{Unscaled position of y.}
+#'   \item{glyph_id}{IDs to distinguish the glyphs.}
+#'   \item{path_id}{IDs to distinguish the groups of paths.}
+#'   \item{triangle_id}{IDs to distinguish the triangles. `string2path()` doesn't contain this column.}
+#' }
 #'
 #' @examples
 #' if (requireNamespace("systemfonts", quietly = TRUE)) {
 #'   available_fonts <- systemfonts::system_fonts()$path
 #'
+#'   # string2path supports only TTF or OTF formats
 #'   ttf_or_otf <- available_fonts[grepl("\\.(ttf|otf)$", available_fonts)]
 #'
+#'   # string2path() converts a text to paths
 #'   d_path <- string2path("TEXT", ttf_or_otf[1])
 #'   plot(d_path$x, d_path$y)
-#'   lines(d_path$x, d_path$y)
+#'   for (p in split(d_path, d_path$path_id)) {
+#'     lines(p$x, p$y)
+#'   }
+#'
+#'   # string2stroke() converts a text to strokes
+#'   d_stroke <- string2stroke("TEXT", ttf_or_otf[1], line_width = 50)
+#'   plot(d_stroke$x, d_stroke$y)
+#'
+#'   # The stroke is split into triangles, which can be distinguished by `triangle_id`
+#'   set.seed(2)
+#'   for (p in split(d_stroke, d_stroke$triangle_id)) {
+#'     polygon(p$x, p$y, col = rgb(runif(1), runif(1), runif(1), 0.8))
+#'   }
+#'
+#'   # string2fill() converts a text to filled polygons
+#'   d_fill <- string2fill("TEXT", ttf_or_otf[1])
+#'   plot(d_fill$x, d_fill$y)
+#'
+#'   # The polygon is split into triangles, which can be distinguished by `triangle_id`
+#'   set.seed(2)
+#'   for (p in split(d_fill, d_fill$triangle_id)) {
+#'     polygon(p$x, p$y, col = rgb(runif(1), runif(1), runif(1), 0.8))
+#'   }
 #' }
 #'
 #' @export
