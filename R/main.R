b8fe360915d3371a1b326b0a54d8e13260f3238f
Hiroaki Yutani <yutani.ini@gmail.com>
2021-07-31 21:09:10 +0900

    Fix YAML

--- R/main.R
+++ R/main.R
@@ -1,26 +1,25 @@
 #' Convert a string to paths.
 #'
-#' `string2path()` converts a text to
+#' `string2path()` converts a text to the paths of the 'width-less' outlines of
+#' each glyph. `string2stroke()` converts a text to the paths of the outlines,
+#' with the specified line width, of each glyph. `string2fill()` converts a text
+#' to the paths of the filled polygon of each glyph.
 #'
 #' @name string2path
-#' @param text
-#'   A text to convert to paths.
-#' @param font_file
-#'   A path to TTF or OTF file.
-#' @param tolerance
-#'   Maximum distance allowed between the curve and its approximation. For more details,
-#'   please refer to [the documentation of the underlying Rust library](https://docs.rs/lyon/0.17.5/lyon/#what-is-the-tolerance-variable-in-these-examples).
-#' @param line_width
-#'   Line width of strokes.
+#' @param text A text to convert to paths.
+#' @param font_file A path to TTF or OTF file.
+#' @param tolerance Maximum distance allowed between the curve and its
+#'   approximation. For more details, please refer to [the documentation of the
+#'   underlying Rust
+#'   library](https://docs.rs/lyon/0.17.5/lyon/#what-is-the-tolerance-variable-in-these-examples).
 #'
-#' @return A `tibble()` containing these columns:
-#' \itemize{
-#'   \item{x}{Unscaled position of x.}
-#'   \item{y}{Unscaled position of y.}
-#'   \item{glyph_id}{IDs to distinguish the glyphs.}
-#'   \item{path_id}{IDs to distinguish the groups of paths.}
-#'   \item{triangle_id}{IDs to distinguish the triangles. `string2path()` doesn't contain this column.}
-#' }
+#' @param line_width Line width of strokes.
+#'
+#' @return A `tibble()` containing these columns: \itemize{ \item{x}{Unscaled
+#'   position of x.} \item{y}{Unscaled position of y.} \item{glyph_id}{IDs to
+#'   distinguish the glyphs.} \item{path_id}{IDs to distinguish the groups of
+#'   paths.} \item{triangle_id}{IDs to distinguish the triangles.
+#'   `string2path()` doesn't contain this column.} }
 #'
 #' @examples
 #' if (requireNamespace("systemfonts", quietly = TRUE)) {
