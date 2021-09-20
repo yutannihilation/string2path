f16766778fb66e9bca136b8207ea5d696063d04f
Hiroaki Yutani <yutani.ini@gmail.com>
2021-09-21 00:46:03 +0900

    Improve configure setups (#17)

---
+++ tools/download_precompiled_binary.R
@@ -0,0 +1,12 @@
+args <- commandArgs(TRUE)
+
+if (length(args) != 2) {
+  stop("Usage: download_precompiled_binary.R URL DESTFILE")
+}
+
+URL <- args[1]
+DESTFILE <- args[2]
+
+dir.create(dirname(DESTFILE), showWarnings = FALSE, recursive = TRUE)
+
+download.file(URL, destfile = DESTFILE, mode = "wb", quiet = TRUE)
