c065384544200e6a062d3973fce97c12ad8a5e03
Hiroaki Yutani <yutani.ini@gmail.com>
2021-09-20 18:15:36 +0900

    Use R script

---
+++ tools/download_precompiled_binary.R
@@ -0,0 +1,10 @@
+args <- commandArgs(TRUE)
+
+if (length(args) != 2) {
+  stop("Usage: download_precompiled_binary.R URL DESTFILE")
+}
+
+URL <- args[1]
+DESTFILE <- args[2]
+
+dir.create(dirname(DESTFILE), showWarnings = FALSE, recursive = TRUE)
