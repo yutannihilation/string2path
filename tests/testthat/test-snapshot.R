7abf0d241fea975f98ebb67db43e1a48aa8bf0bb
Hiroaki Yutani <yutani.ini@gmail.com>
2022-07-17 01:20:43 +0900

    Tweak

--- tests/testthat/test-snapshot.R
+++ tests/testthat/test-snapshot.R
@@ -1,4 +1,6 @@
 test_that("the extracted data are expected", {
+  skip_if_not(isTRUE("Arial" %in% dump_fontdb()$family))
+
   expect_snapshot(string2path("A", "Arial"))
   expect_snapshot(string2stroke("A", "Arial"))
   expect_snapshot(string2fill("A", "Arial"))
