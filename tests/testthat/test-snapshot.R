f25498ebe151c828e1e0f4bf3609421961c1910d
Hiroaki Yutani <yutani.ini@gmail.com>
2022-07-17 00:55:35 +0900

    Update snapshot tests

--- tests/testthat/test-snapshot.R
+++ tests/testthat/test-snapshot.R
@@ -1,5 +1,5 @@
 test_that("the extracted data are expected", {
-  expect_snapshot(string2path("A", "./font/test.ttf"))
-  expect_snapshot(string2stroke("A", "./font/test.ttf"))
-  expect_snapshot(string2fill("A", "./font/test.ttf"))
+  expect_snapshot(string2path("A", "Arial"))
+  expect_snapshot(string2stroke("A", "Arial"))
+  expect_snapshot(string2fill("A", "Arial"))
 })
