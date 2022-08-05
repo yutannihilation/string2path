897c2126041b1d477091c22bbcddfc39bc3fc1ea
Hiroaki Yutani <yutani.ini@gmail.com>
2022-08-06 02:13:37 +0900

    Vendoring (#35)

---
+++ src/rust/vendor.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+cargo vendor
+
+# c.f. https://reproducible-builds.org/docs/archives/
+tar \
+  --sort=name \
+  --mtime='1970-01-01 00:00:00Z' \
+  --owner=0 \
+  --group=0 \
+  --numeric-owner \
+  --xz \
+  --create \
+  --file=vendor.tar.xz \
+  vendor
+
+echo
+echo
+echo "#############################################"
+echo "#                                           #"
+echo "#  UPDATE src/cargo_vendor_config.toml !!!  #"
+echo "#                                           #"
+echo "#############################################"
