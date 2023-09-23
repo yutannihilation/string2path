9622581fba876e78d775b5239711af79048638fd
Hiroaki Yutani <yutani.ini@gmail.com>
2023-09-23 14:52:48 +0900

    Update savvy to v0.1.8

--- src/init.c
+++ src/init.c
@@ -33,50 +33,58 @@ SEXP handle_result(SEXP res_) {
 }
 
 
-SEXP savvy_string2path_family_wrapper(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance) {
-    SEXP res = savvy_string2path_family(text, font_family, font_weight, font_style, tolerance);
+
+
+
+SEXP string2path_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance) {
+    SEXP res = string2path_family(text, font_family, font_weight, font_style, tolerance);
     return handle_result(res);
 }
 
-SEXP savvy_string2path_file_wrapper(SEXP text, SEXP font_file, SEXP tolerance) {
-    SEXP res = savvy_string2path_file(text, font_file, tolerance);
+SEXP string2path_file__impl(SEXP text, SEXP font_file, SEXP tolerance) {
+    SEXP res = string2path_file(text, font_file, tolerance);
     return handle_result(res);
 }
 
-SEXP savvy_string2stroke_family_wrapper(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance, SEXP line_width) {
-    SEXP res = savvy_string2stroke_family(text, font_family, font_weight, font_style, tolerance, line_width);
+SEXP string2stroke_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance, SEXP line_width) {
+    SEXP res = string2stroke_family(text, font_family, font_weight, font_style, tolerance, line_width);
     return handle_result(res);
 }
 
-SEXP savvy_string2stroke_file_wrapper(SEXP text, SEXP font_file, SEXP tolerance, SEXP line_width) {
-    SEXP res = savvy_string2stroke_file(text, font_file, tolerance, line_width);
+SEXP string2stroke_file__impl(SEXP text, SEXP font_file, SEXP tolerance, SEXP line_width) {
+    SEXP res = string2stroke_file(text, font_file, tolerance, line_width);
     return handle_result(res);
 }
 
-SEXP savvy_string2fill_family_wrapper(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance) {
-    SEXP res = savvy_string2fill_family(text, font_family, font_weight, font_style, tolerance);
+SEXP string2fill_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance) {
+    SEXP res = string2fill_family(text, font_family, font_weight, font_style, tolerance);
     return handle_result(res);
 }
 
-SEXP savvy_string2fill_file_wrapper(SEXP text, SEXP font_file, SEXP tolerance) {
-    SEXP res = savvy_string2fill_file(text, font_file, tolerance);
+SEXP string2fill_file__impl(SEXP text, SEXP font_file, SEXP tolerance) {
+    SEXP res = string2fill_file(text, font_file, tolerance);
     return handle_result(res);
 }
 
-SEXP savvy_dump_fontdb_impl_wrapper() {
-    SEXP res = savvy_dump_fontdb_impl();
+SEXP dump_fontdb_impl__impl() {
+    SEXP res = dump_fontdb_impl();
     return handle_result(res);
 }
+
 
 
 static const R_CallMethodDef CallEntries[] = {
-    {"savvy_string2path_family", (DL_FUNC) &savvy_string2path_family_wrapper, 5},
-    {"savvy_string2path_file", (DL_FUNC) &savvy_string2path_file_wrapper, 3},
-    {"savvy_string2stroke_family", (DL_FUNC) &savvy_string2stroke_family_wrapper, 6},
-    {"savvy_string2stroke_file", (DL_FUNC) &savvy_string2stroke_file_wrapper, 4},
-    {"savvy_string2fill_family", (DL_FUNC) &savvy_string2fill_family_wrapper, 5},
-    {"savvy_string2fill_file", (DL_FUNC) &savvy_string2fill_file_wrapper, 3},
-    {"savvy_dump_fontdb_impl", (DL_FUNC) &savvy_dump_fontdb_impl_wrapper, 0},
+
+
+
+
+    {"string2path_family__impl", (DL_FUNC) &string2path_family__impl, 5},
+    {"string2path_file__impl", (DL_FUNC) &string2path_file__impl, 3},
+    {"string2stroke_family__impl", (DL_FUNC) &string2stroke_family__impl, 6},
+    {"string2stroke_file__impl", (DL_FUNC) &string2stroke_file__impl, 4},
+    {"string2fill_family__impl", (DL_FUNC) &string2fill_family__impl, 5},
+    {"string2fill_file__impl", (DL_FUNC) &string2fill_file__impl, 3},
+    {"dump_fontdb_impl__impl", (DL_FUNC) &dump_fontdb_impl__impl, 0},
 
     {NULL, NULL, 0}
 };
