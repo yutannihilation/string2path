803a134c20dd9afd2c78f796d0c06a4e164e2ba3
Hiroaki Yutani <yutani.ini@gmail.com>
2024-01-09 01:32:14 +0900

    Remove LIBRSYS_R_VERSION as this no longer depends on libR-sys

--- tools/print_r_version.R
+++
@@ -1,3 +0,0 @@
-devel_prefix <- if(isTRUE(grepl("devel", R.version$status, fixed = TRUE))) "-devel" else ""
-r_version <- sprintf("%s.%s%s", R.version$major, R.version$minor, devel_prefix)
-cat(r_version)
