f1f6a3eba5f05d63bf15c0dd40926c4cdedbcadb
Hiroaki Yutani <yutani.ini@gmail.com>
2024-04-20 19:39:02 +0900

    Update savvy

--- src/init.c
+++ src/init.c
@@ -1,6 +1,8 @@
 
 #include <stdint.h>
 #include <Rinternals.h>
+#include <R_ext/Parse.h>
+
 #include "rust/api.h"
 
 static uintptr_t TAGGED_POINTER_MASK = (uintptr_t)1;
@@ -32,60 +34,50 @@ SEXP handle_result(SEXP res_) {
     return (SEXP)res;
 }
 
-
-
-
-
-SEXP string2path_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance) {
-    SEXP res = string2path_family(text, font_family, font_weight, font_style, tolerance);
+SEXP savvy_string2path_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance) {
+    SEXP res = savvy_string2path_family__ffi(text, font_family, font_weight, font_style, tolerance);
     return handle_result(res);
 }
 
-SEXP string2path_file__impl(SEXP text, SEXP font_file, SEXP tolerance) {
-    SEXP res = string2path_file(text, font_file, tolerance);
+SEXP savvy_string2path_file__impl(SEXP text, SEXP font_file, SEXP tolerance) {
+    SEXP res = savvy_string2path_file__ffi(text, font_file, tolerance);
     return handle_result(res);
 }
 
-SEXP string2stroke_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance, SEXP line_width) {
-    SEXP res = string2stroke_family(text, font_family, font_weight, font_style, tolerance, line_width);
+SEXP savvy_string2stroke_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance, SEXP line_width) {
+    SEXP res = savvy_string2stroke_family__ffi(text, font_family, font_weight, font_style, tolerance, line_width);
     return handle_result(res);
 }
 
-SEXP string2stroke_file__impl(SEXP text, SEXP font_file, SEXP tolerance, SEXP line_width) {
-    SEXP res = string2stroke_file(text, font_file, tolerance, line_width);
+SEXP savvy_string2stroke_file__impl(SEXP text, SEXP font_file, SEXP tolerance, SEXP line_width) {
+    SEXP res = savvy_string2stroke_file__ffi(text, font_file, tolerance, line_width);
     return handle_result(res);
 }
 
-SEXP string2fill_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance) {
-    SEXP res = string2fill_family(text, font_family, font_weight, font_style, tolerance);
+SEXP savvy_string2fill_family__impl(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance) {
+    SEXP res = savvy_string2fill_family__ffi(text, font_family, font_weight, font_style, tolerance);
     return handle_result(res);
 }
 
-SEXP string2fill_file__impl(SEXP text, SEXP font_file, SEXP tolerance) {
-    SEXP res = string2fill_file(text, font_file, tolerance);
+SEXP savvy_string2fill_file__impl(SEXP text, SEXP font_file, SEXP tolerance) {
+    SEXP res = savvy_string2fill_file__ffi(text, font_file, tolerance);
     return handle_result(res);
 }
 
-SEXP dump_fontdb_impl__impl(void) {
-    SEXP res = dump_fontdb_impl();
+SEXP savvy_dump_fontdb_impl__impl(void) {
+    SEXP res = savvy_dump_fontdb_impl__ffi();
     return handle_result(res);
 }
-
 
 
 static const R_CallMethodDef CallEntries[] = {
-
-
-
-
-    {"string2path_family__impl", (DL_FUNC) &string2path_family__impl, 5},
-    {"string2path_file__impl", (DL_FUNC) &string2path_file__impl, 3},
-    {"string2stroke_family__impl", (DL_FUNC) &string2stroke_family__impl, 6},
-    {"string2stroke_file__impl", (DL_FUNC) &string2stroke_file__impl, 4},
-    {"string2fill_family__impl", (DL_FUNC) &string2fill_family__impl, 5},
-    {"string2fill_file__impl", (DL_FUNC) &string2fill_file__impl, 3},
-    {"dump_fontdb_impl__impl", (DL_FUNC) &dump_fontdb_impl__impl, 0},
-
+    {"savvy_string2path_family__impl", (DL_FUNC) &savvy_string2path_family__impl, 5},
+    {"savvy_string2path_file__impl", (DL_FUNC) &savvy_string2path_file__impl, 3},
+    {"savvy_string2stroke_family__impl", (DL_FUNC) &savvy_string2stroke_family__impl, 6},
+    {"savvy_string2stroke_file__impl", (DL_FUNC) &savvy_string2stroke_file__impl, 4},
+    {"savvy_string2fill_family__impl", (DL_FUNC) &savvy_string2fill_family__impl, 5},
+    {"savvy_string2fill_file__impl", (DL_FUNC) &savvy_string2fill_file__impl, 3},
+    {"savvy_dump_fontdb_impl__impl", (DL_FUNC) &savvy_dump_fontdb_impl__impl, 0},
     {NULL, NULL, 0}
 };
 
--- src/rust/api.h
+++ src/rust/api.h
@@ -1,7 +1,7 @@
-SEXP string2path_family(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance);
-SEXP string2path_file(SEXP text, SEXP font_file, SEXP tolerance);
-SEXP string2stroke_family(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance, SEXP line_width);
-SEXP string2stroke_file(SEXP text, SEXP font_file, SEXP tolerance, SEXP line_width);
-SEXP string2fill_family(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance);
-SEXP string2fill_file(SEXP text, SEXP font_file, SEXP tolerance);
-SEXP dump_fontdb_impl(void);
+SEXP savvy_string2path_family__ffi(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance);
+SEXP savvy_string2path_file__ffi(SEXP text, SEXP font_file, SEXP tolerance);
+SEXP savvy_string2stroke_family__ffi(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance, SEXP line_width);
+SEXP savvy_string2stroke_file__ffi(SEXP text, SEXP font_file, SEXP tolerance, SEXP line_width);
+SEXP savvy_string2fill_family__ffi(SEXP text, SEXP font_family, SEXP font_weight, SEXP font_style, SEXP tolerance);
+SEXP savvy_string2fill_file__ffi(SEXP text, SEXP font_file, SEXP tolerance);
+SEXP savvy_dump_fontdb_impl__ffi(void);
