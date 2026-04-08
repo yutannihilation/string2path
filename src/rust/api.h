1f1751537fae65c706fb49cf4b40f8f6c31a2f9c
Hiroaki Yutani <yutani.ini@gmail.com>
2026-04-08 22:18:12 +0900

    Update savvy to v0.9.4

--- src/rust/api.h
+++ src/rust/api.h
@@ -1,7 +1,7 @@
+SEXP savvy_dump_fontdb_impl__ffi(void);
+SEXP savvy_string2fill_family__ffi(SEXP c_arg__text, SEXP c_arg__font_family, SEXP c_arg__font_weight, SEXP c_arg__font_style, SEXP c_arg__tolerance);
+SEXP savvy_string2fill_file__ffi(SEXP c_arg__text, SEXP c_arg__font_file, SEXP c_arg__tolerance);
 SEXP savvy_string2path_family__ffi(SEXP c_arg__text, SEXP c_arg__font_family, SEXP c_arg__font_weight, SEXP c_arg__font_style, SEXP c_arg__tolerance);
 SEXP savvy_string2path_file__ffi(SEXP c_arg__text, SEXP c_arg__font_file, SEXP c_arg__tolerance);
 SEXP savvy_string2stroke_family__ffi(SEXP c_arg__text, SEXP c_arg__font_family, SEXP c_arg__font_weight, SEXP c_arg__font_style, SEXP c_arg__tolerance, SEXP c_arg__line_width);
 SEXP savvy_string2stroke_file__ffi(SEXP c_arg__text, SEXP c_arg__font_file, SEXP c_arg__tolerance, SEXP c_arg__line_width);
-SEXP savvy_string2fill_family__ffi(SEXP c_arg__text, SEXP c_arg__font_family, SEXP c_arg__font_weight, SEXP c_arg__font_style, SEXP c_arg__tolerance);
-SEXP savvy_string2fill_file__ffi(SEXP c_arg__text, SEXP c_arg__font_file, SEXP c_arg__tolerance);
-SEXP savvy_dump_fontdb_impl__ffi(void);
